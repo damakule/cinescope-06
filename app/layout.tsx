5dcce8a042847b4daba5657325c83eddd707c01c
damakule <damakule65@gmail.com>
2026-01-07 12:19:06 +0530

    chore: add tailwind basics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import type { ReactNode } from "react";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { inter, roboto_mono } from "./font";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // Metadata for the cinescope app
 export const metadata: Metadata = {
@@ -22,16 +10,22 @@ export const metadata: Metadata = {
   description: "Your gateway to cinematic insights",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en" data-theme="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${roboto_mono.variable} antialiased font-inter`}
+        className={`${inter.variable} ${roboto_mono.variable} antialiased font-inter`}
       >
         <div className="flex flex-col min-h-screen">
           <Header />
+          {/* <Header /> */}
           {children}
           <Footer />
+          {/* <Footer /> */}
         </div>
       </body>
     </html>
